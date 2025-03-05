962fc9e9320b242142c52cc973dde877e1d092bc
Chan <79136087+hchanhi@users.noreply.github.com>
2025-03-05 10:10:37 +0900

    Update cancel.js

--- public/cancel.js
+++ public/cancel.js
@@ -106,7 +106,7 @@ window.onload = function() {
       function requestCancel() {
           // 서버로 요청 보내기
           $.ajax({
-            url: '/sendCancel', // Node.js 서버로 요청
+            url: '/api/sendCancel', // Node.js 서버로 요청
             type: 'POST',
             contentType: 'application/json; charset=UTF-8',
             data: JSON.stringify({
