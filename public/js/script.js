cbc5401068ba6b607d8caeedda45a9453712c985
Chan <79136087+hchanhi@users.noreply.github.com>
2025-03-18 08:47:51 +0900

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $.ajax({
     url: 'menu.html',
-    success: function(data) {
+    cache: false,
+    success: function (data) {
       $('#menu-placeholder').html(data);
+
+      // 메뉴 로딩 후 현재 페이지 감지
+      const currentPath = window.location.pathname.replace(/\/$/, "");
+      const menuLinks = document.querySelectorAll(".menu-link");
+
+      menuLinks.forEach((link) => {
+        const linkPath = link.getAttribute("href").replace(/\/$/, "");
+        console.log("현재 경로:", currentPath);
+        console.log("링크 경로:", linkPath);
+
+        if (linkPath === currentPath) {
+          link.classList.add("active");
+        }
+      });
     },
-    error: function(xhr, status, error) {
+    error: function (xhr, status, error) {
       console.error('메뉴바 로딩 실패:', error);
-    }
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  const currentPath = window.location.pathname.replace(/\/$/, "");
-  const menuLinks = document.querySelectorAll(".menu-link");
-  menuLinks.forEach((link) => {
-    const linkPath = link.getAttribute("href").replace(/\/$/, ""); 
-
-    console.log(currentPath);
-    console.log(link.getAttribute("href"));
-    
-    if (linkPath === currentPath) {
-      link.classList.add("active");
     }
   });
 });
