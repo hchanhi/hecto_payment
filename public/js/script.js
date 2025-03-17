1bef9ef358097194d8f18677a556eb5f69a83198
Chan <79136087+hchanhi@users.noreply.github.com>
2025-03-18 08:58:55 +0900

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -5,15 +5,11 @@ $(document).ready(function () {
     success: function (data) {
       $('#menu-placeholder').html(data);
 
-      // 메뉴 로딩 후 현재 페이지 감지
       const currentPath = window.location.pathname.replace(/\/$/, "");
       const menuLinks = document.querySelectorAll(".menu-link");
 
       menuLinks.forEach((link) => {
         const linkPath = link.getAttribute("href").replace(/\/$/, "");
-        console.log("현재 경로:", currentPath);
-        console.log("링크 경로:", linkPath);
-
         if (linkPath === currentPath) {
           link.classList.add("active");
         }
